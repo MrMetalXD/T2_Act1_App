9b9d8b0557e4c35bba3d4ddaa5be50739afe6e84
Alan Cuevas <162539480+MrMetalXD@users.noreply.github.com>
2024-09-11 21:12:52 -0700

    Update PromesasBasicas.jsx

--- PromesasBasicas/PromesasBasicas.jsx
+++ PromesasBasicas/PromesasBasicas.jsx
@@ -1,1 +1,29 @@
 
+function MyFunctions() {
+    
+    function fncSumarUno(numero, fcnResultadoCallBack){
+        setTimeout(function(){
+            fcnResultadoCallBack(numero +1);
+        },800);
+    }
+    
+    console.log("Programa de Funciones");
+ 
+    fncSumarUno(5, function(nuevoValor1){
+        fncSumarUno(nuevoValor1, function(nuevoValor2){
+            fncSumarUno(nuevoValor2, function(nuevoValor3){
+                console.log("Resultado:", nuevoValor3);
+            });
+        });
+    });
+    
+    return (
+        <>
+            <div>
+                <h1>Programa de Funciones</h1>
+            </div>
+        </>
+    );
+};
+
+export default MyFunctions;
